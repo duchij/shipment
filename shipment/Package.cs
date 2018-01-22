31fa4962ea39a0a9b254cd21b3874fa06ff19e08
Boris Duchaj <Boris Duchaj>
2018-01-22 19:32:17 +0100

    zase

---
+++ shipment/Package.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shipment
+{
+    class Package :Shipment 
+    {
+
+        public delegate void PackageHandler(Package item);
+        public event PackageHandler packageProcess;
+
+
+        private string name;
+        private string id;
+        private int pcs;
+        private double price;
+
+        public Package(string name,string id, int pcs, double price)
+        {
+            this.name = name;
+            this.id = id;
+            this.pcs = pcs;
+            this.price = price;
+
+        }
+
+        public void packageArrived(Package pkg)
+        {
+            this.
+        }
+
+
+    }
+}
---
+++ shipment/Shipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shipment
+{
+    class Shipment
+    {
+
+        private string companyName;
+
+        private 
+
+
+        public Shipment()
+        {
+
+        }
+
+    }
+}
